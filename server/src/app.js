c73574bc891db50102c98b3dd74a64639ece53ae
malith06 <harshanmalith06@gmail.com>
2026-07-14 17:14:11 +0530

    security(server): add helmet security headers

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const helmet = require("helmet");
 const cookieParser = require("cookie-parser");
 const errorHandler = require("./middleware/error.middleware");
 
@@ -8,6 +9,7 @@ const auth = require("./routes/auth.routes");
 
 const app = express();
 
+app.use(helmet());
 app.use(cors());
 app.use(express.json());
 app.use(cookieParser());
