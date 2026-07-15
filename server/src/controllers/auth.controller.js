5a475413595826034cdb0d289f45be704a7ec6d9
malith06 <harshanmalith06@gmail.com>
2026-07-15 23:02:21 +0530

    feat(auth): implement POST /api/v1/auth/logout

--- server/src/controllers/auth.controller.js
+++ server/src/controllers/auth.controller.js
@@ -115,10 +115,7 @@ exports.getMe = async (req, res, next) => {
 };
 
 exports.logout = async (req, res, next) => {
-  res.cookie("token", "none", {
-    expires: new Date(Date.now() + 10 * 1000),
-    httpOnly: true,
-  });
+  res.clearCookie("token");
 
   res.status(200).json({
     success: true,
