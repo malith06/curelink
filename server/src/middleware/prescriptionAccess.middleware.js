fedb65aa669923982687659a4fa81a6c1ab05fb5
malith06 <harshanmalith06@gmail.com>
2026-07-25 14:03:57 +0530

    security(prescription): grant access to selected pharmacies

--- server/src/middleware/prescriptionAccess.middleware.js
+++ server/src/middleware/prescriptionAccess.middleware.js
@@ -1,4 +1,5 @@
 const Prescription = require("../modules/prescriptions/prescription.model");
+const MedicineRequest = require("../modules/requests/request.model");
 const ApiError = require("../utils/ApiError");
 const asyncHandler = require("../utils/asyncHandler");
 
@@ -18,6 +19,23 @@ exports.checkPrescriptionAccess = asyncHandler(async (req, res, next) => {
     if (prescription.customerId.toString() !== req.user._id.toString()) {
       return next(new ApiError(403, "You do not have permission to access this prescription"));
     }
+  } else if (req.user.role === "PHARMACY") {
+    const request = await MedicineRequest.findById(prescription.requestId);
+    if (!request) {
+      return next(new ApiError(404, "Associated medicine request not found"));
+    }
+    
+    // Check if the pharmacy is in the selected pharmacies array
+    const isSelected = request.selectedPharmacyIds.some(
+      id => id.toString() === req.user._id.toString()
+    );
+
+    if (!isSelected) {
+      return next(new ApiError(403, "Your pharmacy was not selected for this prescription request"));
+    }
+  } else if (req.user.role !== "ADMIN") {
+    // If not Customer, Pharmacy, or Admin, deny access.
+    return next(new ApiError(403, "Unauthorized role for prescription access"));
   }
 
   // We attach the prescription object so downstream controllers don't need to re-fetch
