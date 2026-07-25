0d51b88659624e4f504cc854a0f9295ed3dd7519
malith06 <harshanmalith06@gmail.com>
2026-07-25 13:56:44 +0530

    feat(prescription): expose prescription upload endpoint

---
+++ server/src/modules/prescriptions/prescription.controller.js
@@ -0,0 +1,35 @@
+const asyncHandler = require("../../utils/asyncHandler");
+const prescriptionService = require("./prescription.service");
+const ApiError = require("../../utils/ApiError");
+
+/**
+ * Uploads a prescription and attaches it to a medicine request.
+ */
+exports.uploadPrescription = asyncHandler(async (req, res, next) => {
+  const { requestId } = req.params;
+  const customerId = req.user._id;
+
+  if (!req.file) {
+    return next(new ApiError(400, "Prescription file is required"));
+  }
+
+  const prescription = await prescriptionService.uploadPrescription(
+    customerId,
+    requestId,
+    req.file
+  );
+
+  res.status(201).json({
+    success: true,
+    data: {
+      prescriptionId: prescription._id,
+      requestId: prescription.requestId,
+      safeFileName: prescription.safeFileName,
+      mimeType: prescription.mimeType,
+      fileSizeBytes: prescription.fileSizeBytes,
+      uploadStatus: prescription.uploadStatus,
+      ocrStatus: prescription.ocrStatus,
+      createdAt: prescription.createdAt,
+    },
+  });
+});
