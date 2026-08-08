c2aad4113a6dfbb46d2941f20c9b99ac5123d71d
malith06 <harshanmalith06@gmail.com>
2026-08-08 21:51:25 +0530

    feat(notification): frontend UI components: bell & dropdown

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import 'react-toastify/dist/ReactToastify.css';
 // Context
 import { AuthProvider } from './context/AuthContext';
 import { SocketProvider } from './context/SocketContext';
+import { NotificationProvider } from './context/NotificationContext';
 
 // Components
 import ProtectedRoute from './components/common/ProtectedRoute';
@@ -18,6 +19,7 @@ import HomePage from './pages/HomePage';
 import AboutPage from './pages/AboutPage';
 import NotFoundPage from './pages/NotFoundPage';
 import NearbyPharmacySearchPage from './pages/NearbyPharmacySearchPage';
+import NotificationsPage from './pages/NotificationsPage';
 
 // Auth Pages
 import LoginPage from './pages/auth/LoginPage';
@@ -60,221 +62,233 @@ function App() {
   return (
     <AuthProvider>
       <SocketProvider>
-        <Router>
-          <ToastContainer position="top-right" autoClose={3000} />
-          <Routes>
-            {/* Public Routes wrapped in PublicLayout */}
-            <Route path="/" element={<PublicLayout />}>
-              <Route index element={<HomePage />} />
-              <Route path="about" element={<AboutPage />} />
-              <Route path="login" element={<LoginPage />} />
-              <Route path="/pharmacies/nearby" element={<NearbyPharmacySearchPage />} />
-              <Route path="/register/customer" element={<CustomerRegisterPage />} />
-              <Route path="/register/pharmacy" element={<PharmacyRegisterPage />} />
-              
-              {/* Customer Routes */}
-              <Route 
-                path="/customer/dashboard" 
-                element={
-                  <ProtectedRoute allowedRoles={['CUSTOMER']}>
-                    <CustomerDashboardPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/customer/requests" 
-                element={
-                  <ProtectedRoute allowedRoles={['CUSTOMER']}>
-                    <CustomerRequestsPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/customer/requests/new" 
-                element={
-                  <ProtectedRoute allowedRoles={['CUSTOMER']}>
-                    <CreateRequestPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/customer/requests/:id" 
-                element={
-                  <ProtectedRoute allowedRoles={['CUSTOMER']}>
-                    <RequestDetailsPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/customer/requests/:id/quotations/:quotationId" 
-                element={
-                  <ProtectedRoute allowedRoles={['CUSTOMER']}>
-                    <CustomerQuotationDetailsPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/customer/orders" 
-                element={
-                  <ProtectedRoute allowedRoles={['CUSTOMER']}>
-                    <CustomerOrdersPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/customer/orders/create/:quotationId" 
-                element={
-                  <ProtectedRoute allowedRoles={['CUSTOMER']}>
-                    <CreateOrderPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/customer/orders/:orderId/payment" 
-                element={
-                  <ProtectedRoute allowedRoles={['CUSTOMER']}>
-                    <PaymentSelectionPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/customer/orders/:orderId/payment/success" 
-                element={
-                  <ProtectedRoute allowedRoles={['CUSTOMER']}>
-                    <PaymentSuccessPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/customer/orders/:orderId/payment/cancel" 
-                element={
-                  <ProtectedRoute allowedRoles={['CUSTOMER']}>
-                    <PaymentCancelPage />
-                  </ProtectedRoute>
-                } 
-              />
+        <NotificationProvider>
+          <Router>
+            <ToastContainer position="top-right" autoClose={3000} />
+            <Routes>
+              {/* Public Routes wrapped in PublicLayout */}
+              <Route path="/" element={<PublicLayout />}>
+                <Route index element={<HomePage />} />
+                <Route path="about" element={<AboutPage />} />
+                <Route path="login" element={<LoginPage />} />
+                <Route path="/pharmacies/nearby" element={<NearbyPharmacySearchPage />} />
+                <Route path="/register/customer" element={<CustomerRegisterPage />} />
+                <Route path="/register/pharmacy" element={<PharmacyRegisterPage />} />
+                
+                {/* Customer Routes */}
+                <Route 
+                  path="/customer/dashboard" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER']}>
+                      <CustomerDashboardPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/customer/requests" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER']}>
+                      <CustomerRequestsPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/customer/requests/new" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER']}>
+                      <CreateRequestPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/customer/requests/:id" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER']}>
+                      <RequestDetailsPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/customer/requests/:id/quotations/:quotationId" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER']}>
+                      <CustomerQuotationDetailsPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/customer/orders" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER']}>
+                      <CustomerOrdersPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/customer/orders/create/:quotationId" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER']}>
+                      <CreateOrderPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/customer/orders/:orderId/payment" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER']}>
+                      <PaymentSelectionPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/customer/orders/:orderId/payment/success" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER']}>
+                      <PaymentSuccessPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/customer/orders/:orderId/payment/cancel" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER']}>
+                      <PaymentCancelPage />
+                    </ProtectedRoute>
+                  } 
+                />
 
-              {/* Pharmacy Routes */}
-              <Route 
-                path="/pharmacy/dashboard" 
-                element={
-                  <ProtectedRoute allowedRoles={['PHARMACY']}>
-                    <PharmacyDashboardPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/pharmacy/profile" 
-                element={
-                  <ProtectedRoute allowedRoles={['PHARMACY']}>
-                    <PharmacyProfilePage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/pharmacy/location" 
-                element={
-                  <ProtectedRoute allowedRoles={['PHARMACY']}>
-                    <PharmacyLocationPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/pharmacy/availability" 
-                element={
-                  <ProtectedRoute allowedRoles={['PHARMACY']}>
-                    <PharmacyAvailabilityPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/pharmacy/inbox" 
-                element={
-                  <ProtectedRoute allowedRoles={['PHARMACY']}>
-                    <PharmacyInboxPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/pharmacy/quotations" 
-                element={
-                  <ProtectedRoute allowedRoles={['PHARMACY']}>
-                    <PharmacyQuotationsPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/pharmacy/requests/:id" 
-                element={
-                  <ProtectedRoute allowedRoles={['PHARMACY']}>
-                    <PharmacyRequestDetailsPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/pharmacy/orders" 
-                element={
-                  <ProtectedRoute allowedRoles={['PHARMACY']}>
-                    <PharmacyOrdersPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/pharmacy/orders/:id" 
-                element={
-                  <ProtectedRoute allowedRoles={['PHARMACY']}>
-                    <PharmacyOrderDetailsPage />
-                  </ProtectedRoute>
-                } 
-              />
+                {/* Pharmacy Routes */}
+                <Route 
+                  path="/pharmacy/dashboard" 
+                  element={
+                    <ProtectedRoute allowedRoles={['PHARMACY']}>
+                      <PharmacyDashboardPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/pharmacy/profile" 
+                  element={
+                    <ProtectedRoute allowedRoles={['PHARMACY']}>
+                      <PharmacyProfilePage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/pharmacy/location" 
+                  element={
+                    <ProtectedRoute allowedRoles={['PHARMACY']}>
+                      <PharmacyLocationPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/pharmacy/availability" 
+                  element={
+                    <ProtectedRoute allowedRoles={['PHARMACY']}>
+                      <PharmacyAvailabilityPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/pharmacy/inbox" 
+                  element={
+                    <ProtectedRoute allowedRoles={['PHARMACY']}>
+                      <PharmacyInboxPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/pharmacy/quotations" 
+                  element={
+                    <ProtectedRoute allowedRoles={['PHARMACY']}>
+                      <PharmacyQuotationsPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/pharmacy/requests/:id" 
+                  element={
+                    <ProtectedRoute allowedRoles={['PHARMACY']}>
+                      <PharmacyRequestDetailsPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/pharmacy/orders" 
+                  element={
+                    <ProtectedRoute allowedRoles={['PHARMACY']}>
+                      <PharmacyOrdersPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/pharmacy/orders/:id" 
+                  element={
+                    <ProtectedRoute allowedRoles={['PHARMACY']}>
+                      <PharmacyOrderDetailsPage />
+                    </ProtectedRoute>
+                  } 
+                />
 
-              {/* Admin Routes */}
-              <Route 
-                path="/admin/dashboard" 
-                element={
-                  <ProtectedRoute allowedRoles={['ADMIN']}>
-                    <AdminDashboardPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/admin/pharmacies" 
-                element={
-                  <ProtectedRoute allowedRoles={['ADMIN']}>
-                    <AdminPharmaciesPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/admin/pharmacies/:id" 
-                element={
-                  <ProtectedRoute allowedRoles={['ADMIN']}>
-                    <AdminPharmacyDetailsPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/admin/medicines" 
-                element={
-                  <ProtectedRoute allowedRoles={['ADMIN']}>
-                    <AdminMedicinesPage />
-                  </ProtectedRoute>
-                } 
-              />
-              <Route 
-                path="/admin/payments" 
-                element={
-                  <ProtectedRoute allowedRoles={['ADMIN']}>
-                    <AdminPaymentsPage />
-                  </ProtectedRoute>
-                } 
-              />
-              
-              {/* Catch-all 404 Route */}
-              <Route path="*" element={<NotFoundPage />} />
-            </Route>
-          </Routes>
-        </Router>
+                {/* Admin Routes */}
+                <Route 
+                  path="/admin/dashboard" 
+                  element={
+                    <ProtectedRoute allowedRoles={['ADMIN']}>
+                      <AdminDashboardPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/admin/pharmacies" 
+                  element={
+                    <ProtectedRoute allowedRoles={['ADMIN']}>
+                      <AdminPharmaciesPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/admin/pharmacies/:id" 
+                  element={
+                    <ProtectedRoute allowedRoles={['ADMIN']}>
+                      <AdminPharmacyDetailsPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/admin/medicines" 
+                  element={
+                    <ProtectedRoute allowedRoles={['ADMIN']}>
+                      <AdminMedicinesPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                <Route 
+                  path="/admin/payments" 
+                  element={
+                    <ProtectedRoute allowedRoles={['ADMIN']}>
+                      <AdminPaymentsPage />
+                    </ProtectedRoute>
+                  } 
+                />
+
+                {/* Shared Protected Routes */}
+                <Route 
+                  path="/notifications" 
+                  element={
+                    <ProtectedRoute allowedRoles={['CUSTOMER', 'PHARMACY']}>
+                      <NotificationsPage />
+                    </ProtectedRoute>
+                  } 
+                />
+                
+                {/* Catch-all 404 Route */}
+                <Route path="*" element={<NotFoundPage />} />
+              </Route>
+            </Routes>
+          </Router>
+        </NotificationProvider>
       </SocketProvider>
     </AuthProvider>
   );
